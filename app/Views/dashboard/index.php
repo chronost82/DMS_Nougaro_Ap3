67431ed6569a370c9e41b60f6375a7e6f106bf05
Noann <n.ozier@lyceeclaudenougaro82300.onmicrosoft.com>
2025-10-02 10:29:04 +0200

    💄 Mettre à jour le style et la structure du tableau de bord

--- app/Views/dashboard/index.php
+++ app/Views/dashboard/index.php
@@ -1,11 +1,23 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
+    <link rel="stylesheet" href="css/dashboard.css">
 </head>
+
 <body>
-    
+    <main>
+        <nav class="nav-grid" aria-label="Navigation principale">
+            <a href="">Liste des demandes</a>
+            <a href="">Liste des clients</a>
+            <a href="">Liste des tests</a>
+            <a href="">Demandes validées</a>
+            <a href="">Liste des élèves</a>
+        </nav>
+    </main>
 </body>
+
 </html>
