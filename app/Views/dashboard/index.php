2c92b35e8b2d5cc25543f1d41fd2b1427961a19a
Noann <n.ozier@lyceeclaudenougaro82300.onmicrosoft.com>
2025-10-02 11:58:10 +0200

    ✨ Réorganiser la structure des vues et ajouter des sections pour le tableau de bord et la liste des élèves

--- app/Views/dashboard/index.php
+++ app/Views/dashboard/index.php
@@ -1,23 +1,22 @@
-<!DOCTYPE html>
-<html lang="fr">
+<?php $this->extend('layout') ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard</title>
-    <link rel="stylesheet" href="css/dashboard.css">
-</head>
 
-<body>
-    <main>
-        <nav class="nav-grid" aria-label="Navigation principale">
-            <a href="">Liste des demandes</a>
-            <a href="">Liste des clients</a>
-            <a href="">Liste des tests</a>
-            <a href="">Demandes validées</a>
-            <a href="<?= url_to('admin-liste-eleves')?>">Liste des élèves</a>
-        </nav>
-    </main>
-</body>
+<?= $this->section('meta') ?>
+<link rel="stylesheet" href="css/dashboard.css">
+<?php $this->endSection() ?>
 
-</html>
+
+
+<?= $this->section('content') ?>
+
+<main>
+    <nav class="nav-grid" aria-label="Navigation principale">
+        <a href="<?= url_to('admin-liste-demandes-en-attentes')?>">Liste des demandes</a>
+        <a href="<?= url_to('admin-liste-clients')?>">Liste des clients</a>
+        <a href="<?= url_to('liste-test') ?>">Liste des tests</a>
+        <a href="<?= url_to('suppr-test') ?>">Demandes validées</a>
+        <a href="<?= url_to('admin-liste-eleves') ?>">Liste des élèves</a>
+    </nav>
+</main>
+
+<?= $this->endSection() ?>
