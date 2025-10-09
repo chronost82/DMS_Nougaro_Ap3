c04ee7c2c30d2ac761e7bed602d796b2e2160092
Noann <n.ozier@lyceeclaudenougaro82300.onmicrosoft.com>
2025-10-09 10:47:38 +0200

    ✨ Mettre à jour les routes et améliorer la gestion des demandes

--- app/Controllers/DemandeValidesController.php
+++ app/Controllers/DemandeValidesController.php
@@ -5,10 +5,73 @@ namespace App\Controllers;
 use App\Controllers\BaseController;
 use CodeIgniter\HTTP\ResponseInterface;
 
-class ControllerDemandeValides extends BaseController
+class DemandeValidesController extends BaseController
 {
     public function affiche()
     {
-        //
+        $client = [
+            [
+                "id" => 1,
+                "nom" => "Dupont",
+                "prenom" => "Jean",
+                "email" => "@gmail.com",
+                "heure" => "14:30",
+                "telephone" => "06 12 34 56 78",
+                "marque" => "Yamaha",
+                "modele" => "MT-07",
+                "immatriculation" => "AB-123-CD",
+                "etat" => "valide"
+            ],
+            [
+                "id" => 2,
+                "nom" => "Durand",
+                "prenom" => "Marie",
+                "email" => "@yahoo.fr",
+                "heure" => "16:00",
+                "telephone" => "06 98 76 54 32",
+                "marque" => "Honda",
+                "modele" => "CB500F",
+                "immatriculation" => "EF-456-GH",
+                "etat" => "attente"
+            ],
+            [
+                "id" => 3,
+                "nom" => "Martin",
+                "prenom" => "Paul",
+                "email" => "@hotmail.com",
+                "heure" => "10:00",
+                "telephone" => "06 11 22 33 44",
+                "marque" => "Kawasaki",
+                "modele" => "Ninja 400",
+                "immatriculation" => "IJ-789-KL",
+                "etat" => "termine"
+            ],
+            [
+                "id" => 4,
+                "nom" => "Bernard",
+                "prenom" => "Sophie",
+                "email" => "@orange.fr",
+                "heure" => "11:30",
+                "telephone" => "06 55 66 77 88",
+                "marque" => "Suzuki",
+                "modele" => "GSX-S750",
+                "immatriculation" => "MN-012-OP",
+                "etat" => "attente"
+            ],
+            [
+                "id" => 5,
+                "nom" => "Lefevre",
+                "prenom" => "Luc",
+                "email" => "@free.fr",
+                "heure" => "15:00",
+                "telephone" => "06 99 88 77 66",
+                "marque" => "Ducati",
+                "modele" => "Monster 821",
+                "immatriculation" => "QR-345-ST",
+                "etat" => "valide"
+            ]
+        ];
+        $status = 'valide';
+        return view('Dashboard/GestionDemandes.php', ['clients' => $client, 'status' => $status]);
     }
 }
