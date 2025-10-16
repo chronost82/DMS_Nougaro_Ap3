9a64249ac554bb1257e681d3cd925b3616bfd03b
Noann <n.ozier@lyceeclaudenougaro82300.onmicrosoft.com>
2025-10-16 08:24:27 +0200

    💄 Met à jour la langue du document HTML et corrige le chemin du CSS

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/layout.css">
+    <link rel="stylesheet" href="<?= base_url('css/layout.css') ?>">
     <?= $this->renderSection('meta') ?>
     <title><?= esc($title ?? 'Atelier MV') ?></title>
 </head>
