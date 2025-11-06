92534ae19247d32ac9f258b8b8973c03e731f5a7
Piikachad <anthony46090503@gmail.com>
2025-11-06 09:36:02 +0100

    css du login en cours

--- app/Views/Shield/login.php
+++ app/Views/Shield/login.php
@@ -1,18 +1,15 @@
-<?= $this->extend(config('Auth')->views['layout']) ?>
+<?= $this->extend('layout') ?>
 
 <?= $this->section('title') ?><?= lang('Auth.login') ?> <?= $this->endSection() ?>
 
-<?= $this->section('main') ?>
-
-    <div class="container d-flex justify-content-center p-5">
-        <div class="card col-12 col-md-5 shadow-sm">
-            <div class="card-body">
-                <h5 class="card-title mb-5"><?= lang('Auth.login') ?></h5>
+<?= $this->section('meta') ?>
+<div class="container card" >
+                <h5><?= lang('Auth.login') ?></h5>
 
                 <?php if (session('error') !== null) : ?>
-                    <div class="alert alert-danger" role="alert"><?= esc(session('error')) ?></div>
+                    <div><?= esc(session('error')) ?></div>
                 <?php elseif (session('errors') !== null) : ?>
-                    <div class="alert alert-danger" role="alert">
+                    <div>
                         <?php if (is_array(session('errors'))) : ?>
                             <?php foreach (session('errors') as $error) : ?>
                                 <?= esc($error) ?>
@@ -25,49 +22,45 @@
                 <?php endif ?>
 
                 <?php if (session('message') !== null) : ?>
-                    <div class="alert alert-success" role="alert"><?= esc(session('message')) ?></div>
+                    <div><?= esc(session('message')) ?></div>
                 <?php endif ?>
 
                 <form action="<?= url_to('login') ?>" method="post">
                     <?= csrf_field() ?>
 
                     <!-- Email -->
-                    <div class="form-floating mb-3">
-                        <input type="text" class="form-control" id="floatingEmailInput" name="username" inputmode="email" autocomplete="username" placeholder="<?= lang('Auth.username') ?>" value="<?= old('username') ?>" required>
-                        <label for="floatingEmailInput"><?= lang('Auth.username') ?></label>
+                    <div>
+                        <input type="text" class="form-control" id="usernameInput" name="username" inputmode="username" autocomplete="username" placeholder="<?= lang('Auth.username') ?>" value="<?= old('username') ?>" required>
                     </div>
 
                     <!-- Password -->
-                    <div class="form-floating mb-3">
+                    <div>
                         <input type="password" class="form-control" id="floatingPasswordInput" name="password" inputmode="text" autocomplete="current-password" placeholder="<?= lang('Auth.password') ?>" required>
-                        <label for="floatingPasswordInput"><?= lang('Auth.password') ?></label>
                     </div>
 
                     <!-- Remember me -->
                     <?php if (setting('Auth.sessionConfig')['allowRemembering']): ?>
-                        <div class="form-check">
-                            <label class="form-check-label">
+                        <div>
+                            <label>
                                 <input type="checkbox" name="remember" class="form-check-input" <?php if (old('remember')): ?> checked<?php endif ?>>
                                 <?= lang('Auth.rememberMe') ?>
                             </label>
                         </div>
                     <?php endif; ?>
 
-                    <div class="d-grid col-12 col-md-8 mx-auto m-3">
-                        <button type="submit" class="btn btn-primary btn-block"><?= lang('Auth.login') ?></button>
+                    <div>
+                        <button type="submit" class="btn-primary"><?= lang('Auth.login') ?></button>
                     </div>
 
                     <?php if (setting('Auth.allowMagicLinkLogins')) : ?>
-                        <p class="text-center"><?= lang('Auth.forgotPassword') ?> <a href="<?= url_to('magic-link') ?>"><?= lang('Auth.useMagicLink') ?></a></p>
+                        <p><?= lang('Auth.forgotPassword') ?> <a href="<?= url_to('magic-link') ?>"><?= lang('Auth.useMagicLink') ?></a></p>
                     <?php endif ?>
 
                     <?php if (setting('Auth.allowRegistration')) : ?>
-                        <p class="text-center"><?= lang('Auth.needAccount') ?> <a href="<?= url_to('register') ?>"><?= lang('Auth.register') ?></a></p>
+                        <p><?= lang('Auth.needAccount') ?> <a href="<?= url_to('register') ?>"><?= lang('Auth.register') ?></a></p>
                     <?php endif ?>
 
                 </form>
-            </div>
-        </div>
-    </div>
+</div>
 
 <?= $this->endSection() ?>
