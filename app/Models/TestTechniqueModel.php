f6414069c4c6ba5ddd86ac14fb7f7d464438463c
HiroOkami <celianperrat512@gmail.com>
2025-11-06 23:04:59 +0100

    modification allow fields test technique model

--- app/Models/TestTechniqueModel.php
+++ app/Models/TestTechniqueModel.php
@@ -12,7 +12,7 @@ class TestTechniqueModel extends Model
     protected $returnType       = 'array';
     protected $useSoftDeletes   = false;
     protected $protectFields    = true;
-    protected $allowedFields    = [];
+    protected $allowedFields    = ['LIBELLE'];
 
     protected bool $allowEmptyInserts = false;
     protected bool $updateOnlyChanged = true;
