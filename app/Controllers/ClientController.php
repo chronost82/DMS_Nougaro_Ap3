648e93a1eaf1fdcbade10d1cc76fa124d96fd51a
Noann <n.ozier@lyceeclaudenougaro82300.onmicrosoft.com>
2025-11-13 09:19:13 +0100

    💄 Mettre à jour les champs de formulaire pour une meilleure cohérence

--- app/Controllers/ClientController.php
+++ app/Controllers/ClientController.php
@@ -33,11 +33,11 @@ class ClientController extends BaseController
         $ClientsModel = model('ClientModel');
 
         $clientAModif = [
-            'IDCLIENT' => $this->request->getPost('id'),
+            'IDCLIENT' => $this->request->getPost('idclient'),
             'NOM' => $this->request->getPost('nom'),
             'PRENOM' => $this->request->getPost('prenom'),
-            'TEL' => $this->request->getPost('mail'),
-            'EMAIL' => $this->request->getPost('mail'),
+            'TEL' => $this->request->getPost('tel'),
+            'EMAIL' => $this->request->getPost('email'),
             'NUMRANDOM' => $this->request->getPost('numrandom')
         ];
         $ClientsModel->save($clientAModif);
