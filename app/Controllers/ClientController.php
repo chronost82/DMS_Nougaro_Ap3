c92a781975c40729387cf3c4c4d9920cf6a28f57
chronost82 <ghostaire82@gmail.com>
2025-12-04 10:34:02 +0100

    correction delete client

--- app/Controllers/ClientController.php
+++ app/Controllers/ClientController.php
@@ -20,15 +20,24 @@ class ClientController extends BaseController
         $clientsModel = model('ClientModel');
         $possedeModel = model('PossedeModel');
         $cTModel = model('CTModel');
+        $testModel = model('TestModel');
         $demandeModel = model('Demande');
         $clientPossede = $possedeModel->where('IDCLIENT', $idClient)->find();
-        
-        if (true) {
+        $clientCT = $cTModel->where('IDCT', $clientPossede[0]['IDCT'])->find();
+        $clientTest = $testModel->where('IDCT', $clientPossede[0]['IDCT'])->find();
+        if ($demandeModel->where('IDCLIENT', $idClient)->find()) {
             return redirect("liste-clients")->back()->with('erreur', 'Il reste des demandes liées à ce client veuillez les supprimer avant de supprimer ce client.');
-        }
-        else{
+        } else {
             $possedeModel->delete($idClient);
-            $cTModel->delete($clientPossede[0]['IDCT']);
+            if (
+                $cTModel->where('IDCT', $clientPossede[0]['IDCT'])->find()
+            ) {
+                $cTModel->delete($clientPossede[0]['IDCT']);
+            }
+            if ($testModel->where('IDCT', $clientPossede[0]['IDCT'])->find()) {
+                $testModel->delete($clientTest['IDTESTTECHNIQUE']);
+            }
+            
             $clientsModel->delete($idClient);
             return redirect("liste-clients");
         }
