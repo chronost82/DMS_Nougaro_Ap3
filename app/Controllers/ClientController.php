737e43a3107a4102a3a28c30d713429fc1994614
Célian Perrat <celianperrat512@gmail.com>
2025-12-04 10:41:42 +0100

    Remove redundant client-related queries in delete method

--- app/Controllers/ClientController.php
+++ app/Controllers/ClientController.php
@@ -23,21 +23,12 @@ class ClientController extends BaseController
         $testModel = model('TestModel');
         $demandeModel = model('Demande');
         $clientPossede = $possedeModel->where('IDCLIENT', $idClient)->find();
-<<<<<<< HEAD
         $clientCT = $cTModel->where('IDCT', $clientPossede[0]['IDCT'])->find();
         $clientTest = $testModel->where('IDCT', $clientPossede[0]['IDCT'])->find();
-=======
-
->>>>>>> b8f97b01d3785aa5b3f808432de4cb6f44491d20
         if ($demandeModel->where('IDCLIENT', $idClient)->find()) {
             return redirect("liste-clients")->back()->with('erreur', 'Il reste des demandes liées à ce client veuillez les supprimer avant de supprimer ce client.');
         } else {
             $possedeModel->delete($idClient);
-            if (
-                $cTModel->where('IDCT', $clientPossede[0]['IDCT'])->find()
-            ) {
-                $cTModel->delete($clientPossede[0]['IDCT']);
-            }
             if ($testModel->where('IDCT', $clientPossede[0]['IDCT'])->find()) {
                 $testModel->delete($clientTest['IDTESTTECHNIQUE']);
             }
