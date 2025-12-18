d3b47980f94ae385db3ea67afadf3985acec369a
chronost82 <ghostaire82@gmail.com>
2025-12-18 10:51:46 +0100

    ajout colonne dans élèves changement du système de suppression

--- app/Models/ElevesModel.php
+++ app/Models/ElevesModel.php
@@ -12,7 +12,7 @@ class ElevesModel extends Model
     protected $returnType       = 'array';
     protected $useSoftDeletes   = false;
     protected $protectFields    = true;
-    protected $allowedFields    = ["IDELEVE", "NOM", "PRENOM", "ANNEE"];
+    protected $allowedFields    = ["IDELEVE", "NOM", "PRENOM", "ANNEE","DATE_SUPPRESSION"];
 
     protected bool $allowEmptyInserts = false;
     protected bool $updateOnlyChanged = true;
